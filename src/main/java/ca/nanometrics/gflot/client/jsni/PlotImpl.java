c6229d324d7d402c5f17260fcedc0e19265dc90b
David Easton <djeastonca@gmail.com>
2009-05-26 12:26:29 +0000

    Changed return codes for plot offset retrieval as per comments in http://code.google.com/p/gflot/source/detail?r=34

--- src/main/java/ca/nanometrics/gflot/client/jsni/PlotImpl.java
+++ src/main/java/ca/nanometrics/gflot/client/jsni/PlotImpl.java
@@ -34,83 +34,83 @@ import com.google.gwt.user.client.Element;
 public class PlotImpl {
 
 	static native Plot create(Element container, JavaScriptObject series)/*-{
-																			           var plot = new $wnd.jQuery.plot($wnd.jQuery("#"+container.id), series);
-																			           return plot;
-																			    }-*/;
+																				           var plot = new $wnd.jQuery.plot($wnd.jQuery("#"+container.id), series);
+																				           return plot;
+																				    }-*/;
 
 	static native Plot create(Element container, JavaScriptObject series,
 			JavaScriptObject options)/*-{
-																			           var plot = new $wnd.jQuery.plot($wnd.jQuery("#"+container.id), series, options);
-																			           return plot;
-																			    }-*/;
+																				           var plot = new $wnd.jQuery.plot($wnd.jQuery("#"+container.id), series, options);
+																				           return plot;
+																				    }-*/;
 
 	static native void setData(Plot plot, JavaScriptObject series)/*-{
-																			           plot.setData(series);
-																			    }-*/;
+																				           plot.setData(series);
+																				    }-*/;
 
 	static native void draw(Plot plot)/*-{
-																			           plot.draw();
-																			    }-*/;
+																				           plot.draw();
+																				    }-*/;
 
 	static native void setupGrid(Plot plot)/*-{
-																			           plot.setupGrid();
-																			    }-*/;
+																				           plot.setupGrid();
+																				    }-*/;
 
 	static native void setLinearSelection(Plot plot, double x1, double x2)/*-{
-																			           plot.setSelection({ 'x1': x1, 'x2': x2});
-																			    }-*/;
+																				           plot.setSelection({ 'x1': x1, 'x2': x2});
+																				    }-*/;
 
 	static native void setRectangularSelection(Plot plot, double x1, double y1,
 			double x2, double y2)/*-{
-																			           plot.setSelection({ 'x1': x1, 'y1': y1,  'x2': x2, 'y2': y2});
-																			     }-*/;
+																				           plot.setSelection({ 'x1': x1, 'y1': y1,  'x2': x2, 'y2': y2});
+																				     }-*/;
 
 	static native void addSelectionListener(Element container,
 			SelectionListener listener)/*-{
-																			           $wnd.jQuery("#"+container.id).bind("selected", function(event, area) {
-																			             listener.@ca.nanometrics.gflot.client.event.SelectionListener::selected(DDDD)(area.x1, area.y1, area.x2, area.y2);
-																			           });
-																			    }-*/;
+																				           $wnd.jQuery("#"+container.id).bind("selected", function(event, area) {
+																				             listener.@ca.nanometrics.gflot.client.event.SelectionListener::selected(DDDD)(area.x1, area.y1, area.x2, area.y2);
+																				           });
+																				    }-*/;
 
 	static native void addPlotHoverListener(Element container,
 			PlotHoverListener listener, boolean onlyOnDatapoint, Plot plot)/*-{
-																			           $wnd.jQuery("#"+container.id).bind("plothover", function(event, pos, item) {
-																			              var javaPos = pos==null?null:@ca.nanometrics.gflot.client.PlotPosition::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(pos));
-																			              var javaItem = item==null?null:@ca.nanometrics.gflot.client.PlotItem::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(item));
-																			              if(item != null || !onlyOnDatapoint){
-																			                  listener.@ca.nanometrics.gflot.client.event.PlotHoverListener::onPlotHover(Lca/nanometrics/gflot/client/jsni/Plot;Lca/nanometrics/gflot/client/PlotPosition;Lca/nanometrics/gflot/client/PlotItem;)(plot, javaPos, javaItem);
-																			              }
-																			           });
-																			    }-*/;
+																				           $wnd.jQuery("#"+container.id).bind("plothover", function(event, pos, item) {
+																				              var javaPos = pos==null?null:@ca.nanometrics.gflot.client.PlotPosition::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(pos));
+																				              var javaItem = item==null?null:@ca.nanometrics.gflot.client.PlotItem::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(item));
+																				              if(item != null || !onlyOnDatapoint){
+																				                  listener.@ca.nanometrics.gflot.client.event.PlotHoverListener::onPlotHover(Lca/nanometrics/gflot/client/jsni/Plot;Lca/nanometrics/gflot/client/PlotPosition;Lca/nanometrics/gflot/client/PlotItem;)(plot, javaPos, javaItem);
+																				              }
+																				           });
+																				    }-*/;
 
 	static native void addPlotClickListener(Element container,
 			PlotClickListener listener, boolean onlyOnDatapoint, Plot plot)/*-{
-																			      $wnd.jQuery("#"+container.id).bind("plotclick", function(event, pos, item) {
-																			      var javaPos = pos==null?null:@ca.nanometrics.gflot.client.PlotPosition::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(pos));
-																			      var javaItem = item==null?null:@ca.nanometrics.gflot.client.PlotItem::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(item));
-																			        if(item != null || !onlyOnDatapoint){
-																			          listener.@ca.nanometrics.gflot.client.event.PlotClickListener::onPlotClick(Lca/nanometrics/gflot/client/jsni/Plot;Lca/nanometrics/gflot/client/PlotPosition;Lca/nanometrics/gflot/client/PlotItem;)(plot, javaPos, javaItem);
-																			        }
-																			      });
-																			    }-*/;
+																				      $wnd.jQuery("#"+container.id).bind("plotclick", function(event, pos, item) {
+																				      var javaPos = pos==null?null:@ca.nanometrics.gflot.client.PlotPosition::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(pos));
+																				      var javaItem = item==null?null:@ca.nanometrics.gflot.client.PlotItem::new(Lcom/google/gwt/json/client/JSONObject;)(@com.google.gwt.json.client.JSONObject::new(Lcom/google/gwt/core/client/JavaScriptObject;)(item));
+																				        if(item != null || !onlyOnDatapoint){
+																				          listener.@ca.nanometrics.gflot.client.event.PlotClickListener::onPlotClick(Lca/nanometrics/gflot/client/jsni/Plot;Lca/nanometrics/gflot/client/PlotPosition;Lca/nanometrics/gflot/client/PlotItem;)(plot, javaPos, javaItem);
+																				        }
+																				      });
+																				    }-*/;
 
 	static native int getPlotOffsetLeft(Plot plot)/*-{
-								 var offset = plot.getPlotOffset().left;
-							 return (offset === undefined) ? 0 : offset;
-														}-*/;
+									 var offset = plot.getPlotOffset().left;
+								 return (offset === undefined) ? -1 : offset;
+															}-*/;
 
 	static native int getPlotOffsetRight(Plot plot)/*-{
-		 var offset = plot.getPlotOffset().right;
-	return (offset === undefined) ? 0 : offset;
-								}-*/;
+			 var offset = plot.getPlotOffset().right;
+		return (offset === undefined) ? -1 : offset;
+									}-*/;
 
 	static native int getPlotOffsetTop(Plot plot)/*-{
-		 var offset = plot.getPlotOffset().top;
-	return (offset === undefined) ? 0 : offset;
-								}-*/;
+			 var offset = plot.getPlotOffset().top;
+		return (offset === undefined) ? -1 : offset;
+									}-*/;
 
 	static native int getPlotOffsetBottom(Plot plot)/*-{
-		 var offset = plot.getPlotOffset().bottom;
-	return (offset === undefined) ? 0 : offset;
-								}-*/;
+			 var offset = plot.getPlotOffset().bottom;
+		return (offset === undefined) ? -1 : offset;
+									}-*/;
 }
