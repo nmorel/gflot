e146c10a655505ae550c8de67017faf5b45b2937
Alexander De Leon <alexjdl@gmail.com>
2009-10-14 14:18:04 +0000

    Fixed issue 12

--- src/main/java/ca/nanometrics/gflot/client/PlotPosition.java
+++ src/main/java/ca/nanometrics/gflot/client/PlotPosition.java
@@ -14,10 +14,8 @@ import com.google.gwt.json.client.JSONObject;
  * @author Alexander De Leon
  */
 public class PlotPosition extends JSONObjectWrapper {
-	private static final String X1 = "x1";
-	private static final String Y1 = "y1";
-	private static final String X2 = "x2";
-	private static final String Y2 = "y2";
+	private static final String X = "x";
+	private static final String Y = "y";
 
 	protected PlotPosition() {
 		super();
@@ -27,40 +25,22 @@ public class PlotPosition extends JSONObjectWrapper {
 		super(obj);
 	}
 
-	public PlotPosition(double x1, double y1) {
-		this(new Double(x1), new Double(y1), null, null);
-	}
-
-	public PlotPosition(Double x1, Double y1, Double x2, Double y2) {
+	public PlotPosition(Double x, Double y) {
 		this();
-		if (x1 != null) {
-			put(X1, x1);
+		if (x != null) {
+			put(X, x);
 		}
-		if (y1 != null) {
-			put(Y1, y1);
-		}
-		if (x2 != null) {
-			put(X2, x2);
-		}
-		if (y2 != null) {
-			put(Y2, y2);
+		if (y != null) {
+			put(Y, y);
 		}
 	}
 
-	public Double getX1() {
-		return getDouble(X1);
+	public Double getX() {
+		return getDouble(X);
 	}
 
-	public Double getY1() {
-		return getDouble(Y1);
-	}
-
-	public Double getX2() {
-		return getDouble(X2);
-	}
-
-	public Double getY2() {
-		return getDouble(Y2);
+	public Double getY() {
+		return getDouble(Y);
 	}
 
 }
